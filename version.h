d78a950c389e5c17414bd0edd50617eb85c80ccc
Christian Simon <simon@swine.de>
2014-09-02 15:53:28 +0200

    [TASK] Upstream import of version 1.3.8

---
+++ version.h
@@ -0,0 +1,8 @@
+/* version.h - version define for mini_sendmail */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define MINI_SENDMAIL_VERSION "mini_sendmail/1.3.8 14aug2014"
+
+#endif /* _VERSION_H_ */
